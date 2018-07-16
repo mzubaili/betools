7e8be7102852b2e963c0174e8f9947c27d58a6c8
mzubaili <41269299+mzubaili@users.noreply.github.com>
2018-07-16 21:08:34 +0000

    Add files via upload

---
+++ betools.sh
@@ -0,0 +1,936 @@
+#!/bin/bash
+## auto install tools termux
+# by Zhu Bai Lee
+
+clear
+
+toilet -f shadow -F gay "B3T00L5"
+sleep 1
+toilet -f wideterm -F gay "author: Zhu Bai Lee"
+toilet -f wideterm -F gay "contact: +62 822-1588-5532"
+sleep 1
+echo "\033[32;1m pilih tools yg akan di install :"
+sleep 1
+echo "\033[39;1m"
+echo "1. DT3CT"
+echo "2. L1T3SP4M"
+echo "3. H4MM3R"
+echo "4. X3RX35"
+echo "5. T0R5H4MM3R"
+echo "6. L1T3DD05"
+echo "7. T3RMUX-5TYL1N6"
+echo "8. 4UT0B0T-FB"
+echo "9. B4J1N64Nv6"
+echo "10. W33M4N"
+echo "11. HUNN3R-FR4M3W0RK"
+echo "12. 5QLM4P"
+echo "13. R3C0ND06"
+echo "14. 4DM1N-P4N3L-F1ND3R"
+echo "15. CM4TR1X"
+echo "16. H45H-BUST3R"
+echo "17. UBUNTU"
+echo "18. B0M5P4M"
+echo "19. SP4M-6R4B"
+echo "20. L42YMUX"
+echo "21. D13J0B0U"
+echo "22. M3T45PL01T"
+echo "23. MBF"
+echo "24. NM4P"
+echo "25. 4-R4T"
+echo "26. 1NF0G4"
+echo "27. 4NDR0Z3NM4P"
+echo "28. WTF"
+echo "29. 34SYM4P"
+echo "30. XD3V"
+echo "31. S4NT3T-0NL1N3"
+echo "32. CR1P5"
+echo "33. 51R"
+echo "34. 3V1L-URL"
+echo "35. D555"
+echo "36. R0UT3R5PL01T"
+echo "37. 5L0WL0R15"
+echo "38. G0LD3N-3Y3"
+echo "39. PL4N3T-W0RLD-DD05"
+echo "40. BL4CK-HYDRA"
+echo "41. CUPP"
+echo "42. 1337-H45H"
+echo "43. 1R551"
+echo "44. 1N5T4H4CK"
+echo "45. 1ND0N3S14-WORDL1ST"
+echo "46. W3BD4V"
+echo "47. WP5PL0IT"
+echo "48. W3B5PL01T"
+echo "49. 5QL-L0KM3D"
+echo "50. 20N3S"
+echo "51. C0MM1X"
+echo "52. BRUT4L"
+echo "53. KN0CK-M41L"
+echo "54. 5P4MMER-3M41L"
+echo "55. H4C"
+echo "56. 5H33L"
+echo "57. 5P1D3R-B0T"
+echo "58. N3T-HUNT3R"
+echo "59. FED0R4"
+echo "60. BL4CKB0X"
+echo "61. 3C0D3"
+echo "62. H45H2ER"
+echo "63. P4563NCV4R"
+echo "64. 0W5C4N"
+echo "65. 5P425M5"
+echo "66. K0D0RK"
+echo "67. 5N1TCH"
+echo "68. 051F"
+echo "69. MC"
+echo "70. W1F1-H4CK3R"
+echo "\033[31;1m 0. K3LU4R"
+echo "\033[33;1m 99. info"
+read -p "[ZhuBaiLee@betools]> " act;
+clear
+
+echo '\033[34;1m'
+if [ $act = 1 ] || [ $act = 1 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+python2 d-tect.py
+echo "selesai"
+fi
+
+if [ $act = 2 ] || [ $act = 2 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg update && pkg upgrade
+pkg install git
+pkg install toilet
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+echo "selesai"
+fi
+
+if [ $act = 3 ] || [ $act = 3 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg update && pkg update
+pkg install git
+pkg install python
+git clone https://github.com/cyweb/hammer
+cd hammer
+echo "selesai"
+fi
+
+if [ $act = 4 ] || [ $act = 4 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg update && pkg upgrade
+pkg install git
+pkg install clang
+git clone https://github.com/zanyarjamal/xerxes
+echo "selesai"
+fi
+
+if [ $act = 5 ] || [ $act = 5 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg update && pkg upgrade
+pkg install git
+pkg install tor
+pkg install python2
+git clone https://github.com/dotfighter/torshammer.git
+echo "selesai"
+fi
+
+if [ $act = 6 ] || [ $act = 6 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+echo "selesai"
+fi
+
+if [ $act = 7 ] || [ $act = 7 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install git
+git clone 
+https://gitbub.com/BagazMukti/Termux-Styling-Shell-Script
+echo "selesai"
+fi
+
+if [ $act = 8 ] || [ $act = 8 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install git
+pkg install python2
+git clone https://github.com/Senitopeng/BotFbBangDjon.git
+cd BotFbBangDjon
+python2 bangdjon.py
+echo "selesai"
+fi
+
+if [ $act = 9 ] || [ $act = 9 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install toilet
+pkg install figlet
+pkg install cowsay
+pkg install termcolor
+pkg install ruby
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+echo "selesai"
+fi
+
+if [ $act = 10 ] || = $act = 10 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/evait-security/weeman
+cd weeman
+chmod 777 weeman.py
+python2 weeman.py
+echo "selesai"
+fi
+
+if [ $act = 11 ] || [ $act = 11 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python
+git clone https://github.com/b3-v3r/Hunner
+cd Hunner
+chmod 777 hunner.py
+python hunner.py
+echo "selesai"
+fi
+
+if [ $act = 12 ] || [ $act = 12 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python
+pkg install python2
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+echo "selesai"
+fi
+
+if [ $act = 13 ] || [ $act = 13 ]
+then
+clear
+echo "memginstall..."
+sleep 1
+pkg install python
+pkg install python2
+git clone https://github.com/UltimateHackers/ReconDog
+cd ReconDog
+chmod +x dog.py
+python2 dog.py
+echo "selesai"
+fi
+
+if [ $act = 14 ] || [ $act = 14 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/Techzindia/admin_penal
+cd admin_penal
+chmod +x admin_panel_finder.py
+python2 admin_panel_finder.py
+echo "selesai"
+fi
+
+if [ $act = 15 ] || [ $act = 15 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install cmatrix
+echo "selesai"
+fi
+
+if [ $act = 16 ] || [ $act = 16 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+python2 hash.py
+echo "selesai"
+fi
+
+if [ $act = 17 ] || [ $act = 17 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install wget
+pkg install proot
+git clone https://github.com/Neo-Oli/termux-ubuntu.git
+cd termux-ubuntu
+chmod ubuntu.sh
+sh ubuntu.sh
+echo "selesai"
+fi
+
+if [ $act = 18 ] || [ $act = 18 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+sh install.sh
+sh gco.sh
+echo "selesai"
+fi
+
+if [ $act = 19 ] || [ $act = 19 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pip2 install requests
+git clone https://github.com/p4kl0nc4t/Spammer-Grab
+cd Spammer-Grab
+chmod +x spammer.py
+python2 spammer.py
+echo "selesai"
+fi
+
+if [ $act = 20 ] || [ $act = 20 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+chmod +x lazymux.py
+python2 lazymux.py
+echo "selesai"
+fi
+
+if [ $act = 21 ] || [ $act = 21 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install php
+git clone https://github.com/alintamvanz/diejoubu
+cd diejoubu
+cd v1.2
+php diejoubu.php
+echo "selesai"
+fi
+
+if [ $act = 22 ] || [ $act = 22 ]
+then
+clear
+echo "meninstall..."
+sleep 1
+pkg install curl
+curl -LO 
+https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+chmod +x metasploit.sh
+sh metasploit.sh
+echo "selesai"
+fi
+
+if [ $act = 23 ] || = $act = 23 ]
+then
+clear
+echo "menginstall"
+sleep 1
+pkg install python2
+pip2 install mechanize
+git clone https://github.com/pirmanxs/mbf
+cd mbf
+python2 MBF.py
+echo "selesai"
+fi
+
+if [ $act = 24 ] || [ $act = 24 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install nmap
+echo "selesai"
+fi
+
+if [ $act = 25 ] || [ $act = 25 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/Xi4u7/A-Rat.git
+cd A-Rat
+python2 A-Rat.py
+echo "selesai"
+fi
+
+if [ $act = 26 ] || [ $act = 26 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pip2 install requests urllib3 urlparse
+git clone https://github.com/m4llok/Infoga
+cd Infoga
+ls
+echo "selesai"
+fi
+
+if [ $act = 27 ] || [ $act = 27 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install nmap
+pkg install curl
+curl -O http://override.waper.co/files/androzenmap.txt
+echo "selesai"
+fi
+
+if [ $act = 28 ] || [ $act = 28 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pip2 bs4 requests HTMLParser urlparse mechanize argparse
+git clone https://github.com/Xi4u7/wtf
+cd wtf
+ls
+echo "selesai"
+fi
+
+if [ $act = 29 ] || [ $act = 29 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install php
+git clone https://github.com/Cvar1984/Easymap
+cd Easymap
+sh install.sh
+echo "selesai"
+fi
+
+if [ $act = 30 ] || [ $act = 30 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install curl
+curl -k -O 
+https://gist.github.com/Gameye98/92035588bd0228df6fb7fa77a5f26bc2/raw/f8e73cd3d9f2a72bd536087bb6ba7bc8baef7d1d/xd3v.sh
+echo "selesai"
+fi
+
+if [ $act = 31 ] || [ $act = 31 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pip2 install requests
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+echo "selesai"
+fi
+
+if [ $act = 32 ] || [ $act = 32 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2 openssl curl libcurl wget
+git clone https://github.com/Manisso/Crips
+cd Crips
+ls
+echo "selesai"
+fi
+
+if [ $act = 33 ] || $act = 33 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pip2 install bs4 urllib2
+git clone https://github.com/AeonDave/sir.git
+cd sir
+ls
+echo "selesai"
+fi
+
+if [ $act = 34 ] || [ $act = 34 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pkg install python3
+git clone https://github.com/UndeadSec/EvilURL
+cd EvilURL
+ls
+echo "selesai"
+fi
+
+if [ $act = 35 ] || [ $act = 35 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/stamparm/DSSS
+cd DSSS
+ls
+echo "selesai"
+fi
+
+if [ $act = 36 ] || [ $act = 36 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pip2 install requests
+git clone https://github.com/reverse-shell/routersploit
+cd routersploit
+pip2 install -r requirements.txt termux-fix-shebang rsf.py
+echo "selesai"
+fi
+
+if [ $act = 37 ] || [ $act = 37 ]
+then
+clear
+echo "menginstall... "
+sleep 1
+pkg install python2
+git clone https://github.com/gkbrk/slowloris
+cd slowloris
+echo "selesai"
+fi
+
+if [ $act = 38 ] || [ $act = 38 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/jseidl/GoldenEye
+cd GoldenEye
+echo "selesai"
+fi
+
+if [ $act = 39 ] || [ $act = 39 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/Hydra7/Planetwork-DDOS
+cd Planetwork-DDOS
+echo "selesai"
+fi
+
+if [ $act = 40 ] || [ $act = 40 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install hydra
+pkg install python2
+git clone https://github.com/Gameye98/Black-Hydra
+cd Black-Hydra
+echo "selesai"
+fi
+
+if [ $act = 41 ] || [ $act = 41 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/Mebus/cupp
+cd cupp
+echo "selesai"
+fi
+
+if [ $act = 42 ] || [ $act = 42 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/Gameye98/1337Hash
+cd 1337Hash
+echo "selesai"
+fi
+
+if [ $act = 43 ] || [ $act = 43 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install irssi
+irssi
+echo "selesai"
+fi
+
+if [ $act = 44 ] || [ $act = 44 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pip2 install requests
+git clone https://github.com/avramit/instahack
+cd instahack
+python2 hackinsta.py
+echo "selesai"
+fi
+
+if [ $act = 45 ] || [ $act = 45 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+git clone https://github.com/geovedi/indonesian-wordlist
+echo "selesai"
+fi
+
+if [ $act = 46 ] || [ $act = 46 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+apt install python2 openssl curl libcurl
+pip2 install urllib3 chardet certifi idna requests
+mkdir ~/webdav
+curl -k -O http://override.waper.co/files/webdav.txt;mv 
+webdav.txt ~/webdav/webdav.py
+echo "selesai"
+fi
+
+if [ $act = 47 ] || [ $act = 47 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone git clone https://github.com/m4ll0k/wpsploit
+cd wpsploit
+echo "selesai"
+fi
+
+if [ $act = 48 ] || [ $act = 48 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pip2 install scapy
+git clone https://github.com/The404Hacking/websploit
+cd websploit
+echo "selesai"
+fi
+
+if [ $act = 49 ] || [ $act = 49 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pip2 install urllib2
+git clone https://github.com/Anb3rSecID/sqlokmed
+cd sqlokmed
+fi
+
+if [ $act = 50 ] || [ $act = 50 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install php
+git clone https://github.com/Cvar1984/zones
+cd zones
+fi
+
+if [ $act = 51 ] || [ $act = 51 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/commixproject/commix
+cd commix
+echo "selesai"
+fi
+
+if [ $act = 52 ] || [ $act = 52 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+git clone https://github.com/Screetsec/Brutal
+cd Brutal
+echo "selesai"
+fi
+
+if [ $act = 53 ] || [ $act = 53 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pip2 install validate_email pyDNS
+git clone https://github.com/4w4k3/KnockMail
+cd KnockMail
+echo "selesai"
+fi
+
+if [ $act = 54 ] || [ $act = 54 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+apt install git python2 && pip2 install argparse requests
+git clone https://github.com/p4kl0nc4t/Spammer-Email
+echo "selesai"
+fi
+
+if [ $act = 55 ] || [ $act = 55 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install php
+git clone https://github.com/Cvar1984/Hac
+cd Hac
+echo "selesai"
+fi
+
+if [ $act = 56 ] || [ $act = 56 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/LOoLzeC/SH33LL
+cd SH33LL
+echo "selesai"
+fi
+
+if [ $act = 57 ] || [ $act = 57 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install php
+git clone https://github.com/Cvar1984/SpiderBot
+cd Spider-Bot
+echo "selesai"
+fi
+
+if [ $act = 58 ] || [ $act = 58 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+git clone https://github.com/Hax4us/Nethunter-In-Termux
+echo "selesai"
+fi
+
+if [ $act = 59 ] || [ $act = 59 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install wget
+wget 
+https://raw.githubusercontent.com/nmilosev/termux-fedora/master/termux-fedora.sh
+echo "selesai"
+fi
+
+if [ $act = 60 ] || [ $act = 60 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+apt install python2 git && pip2 install optparse passlib
+git clone https://github.com/jothatron/blackbox
+cd blackbox
+echo "selesai"
+fi
+
+if [ $act = 61 ] || [ $act = 61 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install php
+git clone https://github.com/Cvar1984/Ecode
+cd Ecode
+echo "selesai"
+fi
+
+if [ $act = 62 ] || [ $act = 62 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pip2 install requests 
+git clone https://github.com/Anb3rSecID/Hashzer
+cd Hashzer
+echo "selesai"
+fi
+
+if [ $act = 63 ] || [ $act = 63 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install php
+git clone https://github.com/Cvar1984/PassGen
+cd PassGen
+echo "selesai"
+fi
+
+if [ $act = 64 ] || [ $act = 64 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install php
+git clone https://github.com/Gameye98/OWScan
+cd OWScan
+echo "selesai"
+fi
+
+if [ $act = 65 ] || [ $act = 65 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+apt install git python2 && pip2 install requests
+git clone https://github.com/Gameye98/SpazSMS
+cd SpazSMS
+echo "selesai"
+fi
+
+if [ $act = 66 ] || [ $act = 66 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+apt install git python2 && pip2 install urllib2
+git clone https://github.com/ciku370/ko-dork
+cd ko-dork
+echo "selesai"
+fi
+
+if [ $act = 67 ] || [ $act = 67 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+git clone https://github.com/Smaash/snitch
+cd snitch
+echo "selesai"
+fi
+
+if [ $act = 68] || [ $act = 68 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install python2
+pip2 install requests 
+git clone https://github.com/ciku370/OSIF
+cd OSIF
+echo "selesai"
+fi
+
+if [ $act = 69 ] || [ $act = 69 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+pkg install mc
+mc
+echo "selesai"
+fi
+
+if [ $act = 70 ] || [ $act = 70 ]
+then
+clear
+echo "menginstall..."
+sleep 1
+https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x wifi-hacker.sh
+echo "selesai"
+fi
+
+if [ $act = 99 ] || [ $act = 99 ]
+then
+clear
+echo "INI ADALAH TOOLS AUTO INSTALL, KALAU ADA KESALAHAN HARAP 
+KASIG TAU AKU,  BIAR AKU UPGRADE LAGI."
+echo "DAN JANGAN PERNAH HINA TOOLS INI,  KALO KALIAN GK SUKA 
+BUAT AJA TOOLS SENDIRI"
+sleep 3
+sh betools.sh
+fi
+
+if [ $act = 0 ] || [ $act = 0 ]
+then
+clear
+echo "TUNGGU 3 DETIK UNTUK KELUAR"
+sleep 3
+exit
+echo "selesai"
+fi
+
